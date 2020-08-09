8407911627645104ca36272fb771ba36bb6695f5
Shrish Ankit <dev.shrishankit@gmail.com>
2020-08-09 23:20:21 +0530

    initial checkbox css

---
+++ src/components/Checkbox/checkbox.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="">
+    <div class="">
+      <div class="wrapper">
+        <input class="state" type="radio" name="app" id="a" value="a" />
+        <label class="label" for="a">
+          <div class="indicator"></div>
+          <span class="text">a) close</span>
+        </label>
+      </div>
+      <div class="wrapper">
+        <input class="state" type="radio" name="app" id="b" value="b" />
+        <label class="label" for="b">
+          <div class="indicator"></div>
+          <span class="text">b) remove</span>
+        </label>
+      </div>
+      <div class="wrapper">
+        <input class="state" type="radio" name="app" id="c" value="c" />
+        <label class="label" for="c">
+          <div class="indicator"></div>
+          <span class="text">c) delete</span>
+        </label>
+      </div>
+      <div class="wrapper">
+        <input class="state" type="radio" name="app" id="d" value="d" />
+        <label class="label" for="d">
+          <div class="indicator"></div>
+          <span class="text">d) all of the above</span>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  components: {},
+  props: {},
+  mounted() {
+    this.init();
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    init() {},
+  },
+};
+</script>
--- src/components/testing.vue
+++ src/components/testing.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="hello">
     <div class="fullsize-container home center-imp">
-    <div class="">
-      <p class=" box title">Intro and Documentation</p>
-      <button @click="changeTheme" class="">Here it starts</button>
+      <div class="">
+        <p class=" box title">Intro and Documentation</p>
+        <button @click="changeTheme" class="">Here it starts</button>
+      </div>
+      <checkbox></checkbox>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Testing',
-  props: {
-    msg: String
+  name: "Testing",
+  components: {
+    // HelloWorld
+    checkbox: () =>
+      import(/* webpackChunkName: "about" */ "../components/Checkbox/checkbox"),
   },
-  methods:{
-    changeTheme(){
-      
-      document.documentElement.setAttribute('dark-theme', 'dark')
-    }
-  }
-}
+  props: {
+    msg: String,
+  },
+  methods: {
+    changeTheme() {
+      document.documentElement.setAttribute("dark-theme", "dark");
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
